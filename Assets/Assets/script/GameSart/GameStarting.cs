255fcb174212fe87b7d1ce63a330c87821209fce
44yyp9 <ilovesoccer3939@gmail.com>
2024-09-26 16:45:55 +0900

    [improve]Add_Shuffle_method

--- Assets/Assets/script/GameSart/GameStarting.cs
+++ Assets/Assets/script/GameSart/GameStarting.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class GameStarting : MonoBehaviour
+public class GameStarting : MonoBehaviour,IShuffling
 {
-    // Start is called before the first frame update
+    private int targetValue = 53;
+    [SerializeField] List<GameObject> GameStartingObj= new List<GameObject>();
     void Start()
     {
-        
+        SetActiveObj(false);
+        StartCoroutine(CheckValueCoroutine());
     }
-
-    // Update is called once per frame
-    void Update()
+    public IEnumerator CheckValueCoroutine()
     {
-        
+        while (true)
+        {
+            if (Card.Cards.Count== targetValue)
+            {
+                Shuffle();
+                StartGame();
+                yield break;
+            }
+            yield return null;
+        }
+    }
+    public void Shuffle()
+    {
+        Card.Cards = Card.Cards.OrderBy(a => Guid.NewGuid()).ToList();
+    }
+    private void StartGame()
+    {
+        SetActiveObj(true);
+    }
+    private void SetActiveObj(bool Active)
+    {
+        for(int i = 0; i < GameStartingObj.Count; i++)
+        {
+            GameStartingObj[i].SetActive(Active);
+        }
     }
 }
