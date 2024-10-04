13a1e078d62290d7b362f53fc92a49842332bee4
44yyp9 <ilovesoccer3939@gmail.com>
2024-10-04 11:47:31 +0900

    [Update]Add_Drawing_method

--- Assets/Assets/script/Drawing/Drawing.cs
+++ Assets/Assets/script/Drawing/Drawing.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Drawing : MonoBehaviour
 {
-
+    public void DrawStack(List<Card> MyHandCards)
+    {
+        var TopCard = Card.Cards[0];
+        Card.Cards.RemoveAt(0);
+        MyHandCards.Add(TopCard);
+    }
 }
--- Assets/Assets/script/Drawing/DrawingHandCard.cs
+++ Assets/Assets/script/Drawing/DrawingHandCard.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class DrawingHandCard : MonoBehaviour
+public class DrawingHandCard : MonoBehaviour,IDrawingHandCard
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject enemy;
+    private HandCard EnemyCard;
+    private HandCard PlayerCard;
+    private List<Card> Cards;
+
     void Start()
     {
-        
+        DrawHandCard();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void DrawHandCard()
     {
-        
+        EnemyCard = enemy.GetComponent<HandCard>();
+        PlayerCard = GetComponent<HandCard>();
+        Cards = EnemyCard.HandCards;
+        WaitEnter();
+    }
+    public void WaitEnter()
+    {
+        int _card_position = 0;
+        StartCoroutine(StealCard());
+        IEnumerator StealCard()
+        {
+            while (true)
+            {
+                SelectCard(_card_position);
+                Debug.Log(_card_position);
+                if (Input.GetKeyDown(KeyCode.KeypadEnter))
+                {
+                    EnemyCard.HandCards.RemoveAt(_card_position);
+                    PlayerCard.HandCards.Add(Cards[_card_position]);
+                    yield break;
+                }
+                yield return null;
+            }
+        }
+    }
+    private void SelectCard(int CardPosition)
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            CardPosition -= 1;
+        }else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            CardPosition += 1;
+        }
+        if(CardPosition<0) CardPosition=Cards.Count;
+        if (Cards.Count < CardPosition) CardPosition = 0;
     }
 }
